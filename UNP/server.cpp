4515c7557e6395c7aa87b83df42dac2cc554b479
Jerling <1161676899@qq.com>
2018-11-08 10:01:03 +0800

    1. 修改程序错误，不应该把listen放在循环里 2. 实现获取ip 和端口 3. 将头文件统一放在 unp.h

--- UNP/server.cpp
+++ UNP/server.cpp
@@ -1,12 +1,5 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <assert.h>
-#include <errno.h>
-#include <stdio.h>
 #include <iostream>
+#include "unp.h"
 
 extern int errno;
 
@@ -14,7 +7,6 @@ int main(int argc, char *argv[])
 {
 
     auto skfd = socket(AF_INET, SOCK_STREAM, 0);
-    std::cout << skfd << "\n";
     assert(skfd >= 0);
     struct sockaddr_in addr;
     bzero(&addr, sizeof(addr));
@@ -22,17 +14,24 @@ int main(int argc, char *argv[])
     inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
     addr.sin_port = htons(8088);
     int ret = bind(skfd, (struct sockaddr*)&addr, sizeof(addr));
-    std::cout << ret << "\n";
     if (ret < 0) {
         std::cout << strerror(errno) << std::endl;
     }
     assert(ret != -1);
-    std::cout << "after bind.." << "\n";
 
     int backlog = 5;
+    ret = listen(skfd, backlog);
+    assert(ret != -1);
+    std::cout << "Listening..." << "\n";
+    char buff[500];
+
     while (true) {
-        ret = listen(skfd, backlog);
-        assert(ret != -1);
+        struct sockaddr_in cliaddr;
+        socklen_t len;
+        auto connfd = accept(skfd, (sockaddr *)&cliaddr, &len);
+        std::cout << inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)) << " : " <<  ntohs(cliaddr.sin_port) << std::endl;
+        write(connfd, buff, strlen(buff));
+        close(connfd);
     }
     close( skfd );
     return 0;
---
+++ UNP/unp.h
@@ -0,0 +1,8 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
